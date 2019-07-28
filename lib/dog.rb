11c4a8eafc7501a572463567205bdb9a4809f4b2
Kateryna Strough <katerynastrough@hotmail.com>
2019-07-28 22:45:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -98,7 +98,8 @@ def save
       end.first
       end
 
-
-
-
+      def update 
+        sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+      end 
 end
