b196423aacda406683617c1aeba325bf068c21dd
Kateryna Strough <katerynastrough@hotmail.com>
2019-07-28 22:14:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
-class Dog 
+class Dog
 
-attr_accessor :name, :breed, :id 
+attr_accessor :name, :breed, :id
 
 def initialize(id=nil, name, breed)
-  @id = id 
-  @name = name 
+  @id = id
+  @name = name
   @breed = breed
-end 
+end
 
 
 
